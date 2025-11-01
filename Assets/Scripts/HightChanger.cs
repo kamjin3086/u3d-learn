6fb58cf5b89dcffe3bb7f6512a28a9e5b4401b81
kamjin1996 <kamjin1996@163.com>
2025-11-01 15:13:28 +0800

    inital commit

---
+++ Assets/Scripts/HightChanger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HightChanger : MonoBehaviour
+{
+    public float maxHeight = 5;
+
+    private bool s = false;
+
+    void Start() {
+        s = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(transform.localScale.y > maxHeight) {
+            s = false;
+        }
+
+        if(transform.localScale.y<1){
+            s = true;
+        }
+        
+        if(s) {
+            transform.localScale = new Vector3(1, transform.localScale.y + 0.005f, 1);    
+        } else {
+            transform.localScale = new Vector3(1, transform.localScale.y - 0.002f, 1);    
+        }
+        
+     }
+        
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+
+public GameObject winTextObject;
+
+public float speed = 0;
+
+public TextMeshProUGUI countText;
+
+private int count;
+
+    private Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        winTextObject.SetActive(false);
+        
+    }
+
+    void FixedUpdate(){
+        Vector3 moveDirection = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(moveDirection * speed);
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>();
+        movementX = moveInput.x;
+        movementY = moveInput.y;
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("Pickup")){
+                 other.gameObject.SetActive(false);
+                 count = count + 1;
+                 SetCountText();
+        }       
+    }
+
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.CompareTag("Enemy")){
+            Destroy(gameObject);
+            winTextObject.SetActive(false);
+            countText.text = "You Lose! Count: " + count.ToString();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12){
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+
+            winTextObject.SetActive(true);
+            
+        }
+    }
+}
